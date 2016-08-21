a5ff924a9d7a6801a0ad66952001ca225e2460be
Justin Silver <jsilver@doublesharp.com>
2016-08-21 14:03:24 -0700

    update to use eslint instead of jshint

---
+++ eslint/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "installedESLint": true,
+    "plugins": [
+        "jsx-a11y",
+        "mocha"
+    ],
+    "env": {
+        "es6": true,
+        "node": true
+    }
+};
